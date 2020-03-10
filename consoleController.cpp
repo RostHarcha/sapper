91a6ed0e573853ac4740a780f1fb495dd185bc08
Rostislaww <rostiki.com@mail.ru>
2020-03-10 23:39:43 +0300

    Add files via upload

---
+++ consoleController.cpp
@@ -0,0 +1,24 @@
+#include "settings.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// controller
+class ConsoleController{
+private:
+	std::vector<std::string> command;
+
+public:
+    void use(){
+        std::string input;
+        std::getline(std::cin, input);
+        
+		std::stringstream ss(input);
+		
+		std::string buffer;
+		while(ss >> buffer) command.push_back(buffer)
+		
+		if(command[0] == "flag") std::cout << command[0];
+    }
+};
