3efee81cf497286ac6f0b37bf06da40854dff68d
Rostislaww <rostiki.com@mail.ru>
2020-03-10 20:27:33 +0300

    Update consoleDrawer.h

--- consoleDrawer.h
+++ consoleDrawer.h
@@ -1,10 +1,15 @@
+#include "settings.h"
+
 // view
 class ConsoleDrawer{
 private:
-   
+   Settings set;
 public:
-   
-    void draw(){       
+	void set(Settings _set){
+		set = _set;
+	}
+	
+    void draw(){
         for(int y = 0; y < set.size_y; y++){
             for(int x = 0; x < set.size_x; x++){
                 std::cout << "0" << '\t';
